379a47b5633dfb53eddde3bc4ffca6ae0e9e8be8
nidhijagga <nidhijagga5239@gmail.com>
2023-10-11 14:55:45 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -7,33 +7,62 @@ import './App.css';
 function App() {
   const [movie ,setmovie] =useState([])
   const [isLoading,setisLoading]=useState(false)
-  async function fectchhandler(){
+  const [error,seterror]=useState(null)
+  async function fetchHandler(){
     setisLoading(true)
-  const response=  await fetch('https://swapi.py4e.com/api/films/')
-  const data = await response.json()
+    seterror(null)
+    
+    try{
+      const response=  await fetch('https://swapi.py4e.com/api/film/')
+      if (!response.ok) {
+        throw new Error('Something went wrong ....Retrying')
+        
+      }
+    
+      const data = await response.json()
+      
+          const maindata = data.results.map(item=>{
+            return{
+              id:item.episode_id,
+              title:item.title,
+              openingText:item.opening_crawl,
+              releaseDate:item.release_date
+    
+            }
+          })
+          setmovie(maindata)
+          setisLoading(false)
 
-      const maindata = data.results.map(item=>{
-        return{
-          id:item.episode_id,
-          title:item.title,
-          openingText:item.opening_crawl,
-          releaseDate:item.release_date
-
-        }
-      })
-      setmovie(maindata)
+    }catch(error){
+      seterror(error.message)
       setisLoading(false)
     }
+    }
+
+    let content =<p>No Movie Found</p>
+
+    if (movie.length>0) {
+      content=<MoviesList movies={movie} />
+    }
+
+    if (error) {
+      content = <p>{error}</p>
+    }
+
+    if (isLoading) {
+      content = <p>Loading....</p>
+    }
+    
+  
   
   
   return (
     <React.Fragment>
       <section>
-        <button onClick={fectchhandler}>Fetch Movies</button>
+        <button onClick={fetchHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading&&<MoviesList movies={movie} />}
-        {isLoading&&<p>Loading.......</p>}
+        {content}
       </section>
     </React.Fragment>
   );
